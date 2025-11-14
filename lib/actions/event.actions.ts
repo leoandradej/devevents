6a710644beda442391d3d1996b4f67e6800016b6
Leonardo Andrade <105254884+leoandradej@users.noreply.github.com>
2025-11-13 23:25:38 -0300

    Implement api routes, server actions and caching, and the event details page

---
+++ lib/actions/event.actions.ts
@@ -0,0 +1,21 @@
+"use server";
+
+import { Event } from "@/database";
+import connectDB from "../mongodb";
+
+export const getSimilarEventsBySlug = async (slug: string) => {
+  try {
+    await connectDB();
+
+    const event = await Event.findOne({ slug });
+
+    // MongoDB $ne operator = non equal | $in operator = includes
+    return await Event.find({
+      _id: { $ne: event._id },
+      tags: { $in: event.tags },
+    }).lean();
+  } catch (error) {
+    console.error("Failed to find similar events: ", error);
+    return [];
+  }
+};
