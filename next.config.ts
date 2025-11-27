97afdacf78144b3c2583fa3196fbf2cdb12788ab
Leonardo Andrade <105254884+leoandradej@users.noreply.github.com>
2025-11-27 01:12:09 -0300

    Update nex.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: {
-    ignoreBuildErrors: process.env.NODE_ENV !== "production",
+    ignoreBuildErrors: true,
   },
   cacheComponents: true,
   images: {
